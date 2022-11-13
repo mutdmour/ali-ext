addd7f13e2b805976fb09a6ba23d6bd203cb4ca4
Mutasem <mutdmour@gmail.com>
2022-11-13 18:01:30 +0100

    refactor: try to fix css

--- src/components/CommandPrompt/CommandPrompt.ce.vue
+++ src/components/CommandPrompt/CommandPrompt.ce.vue
@@ -1,9 +1,9 @@
 <script lang="ts" setup>
-import { ref, computed, onMounted, onUnmounted, Ref } from 'vue'
-import actions from '../../actions';
-import type { Result } from '../../../Interface';
+import { ref, computed, onMounted, onUnmounted, Ref } from "vue";
+import actions from "../../actions";
+import type { Result } from "../../../Interface";
 
-const displayCommandPrompt = ref(false)
+const displayCommandPrompt = ref(true);
 const input: Ref<HTMLInputElement | null> = ref(null);
 const searchValue = ref("");
 const selectedIndex = ref(0);
@@ -11,90 +11,93 @@ const selectedIndex = ref(0);
 let allPossibleResults: Result[] = [];
 
 const results = computed(() => {
-	if (searchValue.value.length === 0) {
-		return [];
-	}
-	return allPossibleResults.filter((result) => result.name.toLowerCase().includes(searchValue.value.toLowerCase()));
+  if (searchValue.value.length === 0) {
+    return [];
+  }
+  return allPossibleResults.filter((result) =>
+    result.name.toLowerCase().includes(searchValue.value.toLowerCase())
+  );
 });
 
 function show() {
-	displayCommandPrompt.value = true;
+  displayCommandPrompt.value = true;
 
-	setTimeout(() => {
-		if (input.value) {
-			input.value.focus();
-			input.value.select();
-		}
-	}, 0);
+  setTimeout(() => {
+    if (input.value) {
+      input.value.focus();
+      input.value.select();
+    }
+  }, 0);
 }
 
 function hide() {
-	displayCommandPrompt.value = false;
+  displayCommandPrompt.value = false;
 }
 
 function onKeyboardShortcut(e: KeyboardEvent) {
-	if (e.metaKey && e.shiftKey && e.key === 'k') {
-		show();
-	}
+  if (e.metaKey && e.shiftKey && e.key === "k") {
+    show();
+  }
 }
 
 function submit() {
-	results.value[selectedIndex.value].action();
+  results.value[selectedIndex.value].action();
 }
 
 function setMatches() {
-	const url = window.location.href;
-	const matchingActions = actions.filter((action) => new RegExp(action.urlMatch).test(url));
-	allPossibleResults = [];
-	matchingActions.forEach((action) => {
-		allPossibleResults = [...allPossibleResults, ...action.results];
-	});
+  const url = window.location.href;
+  const matchingActions = actions.filter((action) =>
+    new RegExp(action.urlMatch).test(url)
+  );
+  allPossibleResults = [];
+  matchingActions.forEach((action) => {
+    allPossibleResults = [...allPossibleResults, ...action.results];
+  });
 }
 
 function resetState() {
-	searchValue.value = "";
-	displayCommandPrompt.value = false;
-	setMatches();
+  searchValue.value = "";
+  displayCommandPrompt.value = false;
+  setMatches();
 }
 
 function moveDown() {
-	if (selectedIndex.value < results.value.length - 1) {
-		selectedIndex.value++;
-	}
+  if (selectedIndex.value < results.value.length - 1) {
+    selectedIndex.value++;
+  }
 }
 
 function moveUp() {
-	if (selectedIndex.value > 0) {
-		selectedIndex.value--;
-	}
+  if (selectedIndex.value > 0) {
+    selectedIndex.value--;
+  }
 }
 
 function onKeydown(e: KeyboardEvent) {
-	if (e.key === 'ArrowDown') {
-		e.preventDefault();
-		moveDown();
-	} else if (e.key === 'ArrowUp') {
-		e.preventDefault();
-		moveUp();
-	}
+  if (e.key === "ArrowDown") {
+    e.preventDefault();
+    moveDown();
+  } else if (e.key === "ArrowUp") {
+    e.preventDefault();
+    moveUp();
+  }
 }
 
 onMounted(() => {
-	window.addEventListener('keydown', onKeyboardShortcut);
-	resetState();
+  window.addEventListener("keydown", onKeyboardShortcut);
+  resetState();
 });
 
 onUnmounted(() => {
-	window.removeEventListener('keydown', onKeyboardShortcut);
+  window.removeEventListener("keydown", onKeyboardShortcut);
 });
 </script>
 
 <template>
-<dialog open>
-  <!-- <div class="overlay yo" v-if="displayCommandPrompt" @click="hide"> -->
-    <div class="command-prompt-root">
+  <div class="overlay" v-if="displayCommandPrompt" @click="hide">
+    <div class="command-prompt-root" open>
       <div class="command-prompt" @click.stop>
-        <div :class="{ prompt: true, 'has-results': !!results.length}">
+        <div :class="{ prompt: true, 'has-results': !!results.length }">
           <input
             type="text"
             ref="input"
@@ -114,8 +117,7 @@ onUnmounted(() => {
         </div>
       </div>
     </div>
-  <!-- </div> -->
-	</dialog>
+  </div>
 </template>
 
 <!-- <style module>
@@ -126,6 +128,41 @@ onUnmounted(() => {
 </style> -->
 
 <style>
+:root {
+  --ae-white: #ffffff;
+  --ae-white-soft: #f8f8f8;
+  --ae-white-mute: #f2f2f2;
+
+  --ae-black: #181818;
+  --ae-black-soft: #222222;
+  --ae-black-mute: #282828;
+
+  --ae-text-light-1: var(--ae-indigo);
+  --ae-text-light-2: rgba(60, 60, 60, 0.66);
+  --ae-text-dark-1: var(--ae-white);
+  --ae-text-dark-2: rgba(235, 235, 235, 0.64);
+
+  --ae-highlight: rgb(65, 65, 208);
+
+  --ae-color-background: var(--ae-white);
+  --ae-color-background-soft: var(--ae-white-soft);
+  --ae-color-background-mute: var(--ae-white-mute);
+
+  --ae-color-heading: var(--ae-text-light-1);
+  --ae-color-text: var(--ae-text-light-1);
+}
+
+@media (prefers-color-scheme: dark) {
+  :root {
+    --ae-color-background: var(--ae-black);
+    --ae-color-background-soft: var(--ae-black-soft);
+    --ae-color-background-mute: var(--ae-black-mute);
+
+    --ae-color-heading: var(--ae-text-dark-1);
+    --ae-color-text: var(--ae-text-dark-2);
+  }
+}
+
 *::before,
 *::after {
   box-sizing: border-box;
@@ -198,43 +235,5 @@ onUnmounted(() => {
 .result.selected {
   background-color: var(--ae-highlight);
   opacity: 0.95;
-}
-
-:root {
-  --ae-white: #ffffff;
-  --ae-white-soft: #f8f8f8;
-  --ae-white-mute: #f2f2f2;
-
-  --ae-black: #181818;
-  --ae-black-soft: #222222;
-  --ae-black-mute: #282828;
-
-  --ae-text-light-1: var(--ae-indigo);
-  --ae-text-light-2: rgba(60, 60, 60, 0.66);
-  --ae-text-dark-1: var(--ae-white);
-  --ae-text-dark-2: rgba(235, 235, 235, 0.64);
-
-  --ae-highlight: rgb(65, 65, 208);
-}
-
-/* semantic color variables for this project */
-:root {
-  --ae-color-background: var(--ae-white);
-  --ae-color-background-soft: var(--ae-white-soft);
-  --ae-color-background-mute: var(--ae-white-mute);
-
-  --ae-color-heading: var(--ae-text-light-1);
-  --ae-color-text: var(--ae-text-light-1);
-}
-
-@media (prefers-color-scheme: dark) {
-  :root {
-    --ae-color-background: var(--ae-black);
-    --ae-color-background-soft: var(--ae-black-soft);
-    --ae-color-background-mute: var(--ae-black-mute);
-
-    --ae-color-heading: var(--ae-text-dark-1);
-    --ae-color-text: var(--ae-text-dark-2);
-  }
 }
 </style>
